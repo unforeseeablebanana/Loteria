45794e9175ed6dd5fbea89311c67ece759d4f809
unforeseeablebanana <paola.barkeer@gmail.com>
2025-03-11 13:49:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/loteria/viewModels/ContadorViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.loteria.viewModels
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class ContadorViewModel: ViewModel() {
+    private val _contador = mutableStateOf(0)
+    val contador : State<Int> = _contador
+
+    fun add(){
+        _contador.value = _contador.value + 1
+    }
+}
---
+++ app/src/main/java/com/example/loteria/views/ContadorView.kt
@@ -0,0 +1,51 @@
+package com.example.loteria.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loteria.viewModels.ContadorViewModel
+
+@Composable
+fun Contador(viewModel: ContadorViewModel){
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(
+            text = viewModel.contador.value.toString(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp
+        )
+        FloatingActionButton(
+            onClick = {viewModel.add()},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(15.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "",
+                tint = Color.Blue
+            )
+        }
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+
+        ){}
+    }
+}
